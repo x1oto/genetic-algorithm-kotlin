7d24e826cb11d9815021d503a9d46c7382a48cea
Nick <89790661+x1oto@users.noreply.github.com>
2024-10-15 20:16:56 +0300

    Update Constants.kt

--- Constants.kt
+++ Constants.kt
@@ -1,12 +1,14 @@
 object Constants {
 
+    // Shared constant.
+    const val TIMES = 1000 // Used to determine number of repetitions in each task.
+
+    // Backpack Problem related constants.
     const val POPULATION_SIZE = 10 // Quantity of individuals in our population.
     const val ITEMS_SIZE = 8 // Size must be the same as in indexToBaggageWeight 0..7 (8) elements.
-    const val TIMES = 1000 // Number of generations to produce.
     const val TARGET_WEIGHT = 5950 // Individuals exceeding this weight indicate the required value.
     const val MAX_WEIGHT_LIMIT = 6000 // Maximum weight limit for the items to be transported.
-
-    // Weights associated with each item index.
+    // Weights associated with each item index
     val indexToBaggageWeight = mapOf(
         0 to 400,   // Weight of item 0
         1 to 2200,  // Weight of item 1
@@ -17,4 +19,7 @@ object Constants {
         6 to 850,   // Weight of item 6
         7 to 380    // Weight of item 7
     )
+
+    // Find Target String related constants.
+     const val CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "
 }
